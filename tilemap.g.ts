a9826877b41142a21e39e5ce63bcae0fa7f78c70
Rowan7515 <74774804+Rowan7515@users.noreply.github.com>
2020-12-06 14:39:43 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,5 +3,29 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`100010000c0c0c0c0c0c0c0a0b0c0c0c0c0c0c0c060506050505060809050605050505050506050601010101010101010506060505060501010104020101010401050606060601010101010101010101010105060505010201010101020101010101050506050104010301010101040201010606060501010101040707010101010106060605010101020107070101020101050605060201010101010104010101010606050601010101010103010101030406060606010104020101010201010101050605060501010101040101010101050606050605060101010101010101050506050506050505050608090505060506060505050606060606080906060606060505`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorLight1,sprites.dungeon.floorLight4,sprites.dungeon.floorLightMoss,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.collectibleInsignia,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundEast,sprites.builtin.forestTiles9,sprites.builtin.forestTiles11,sprites.builtin.forestTiles25], TileScale.Sixteen)
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
